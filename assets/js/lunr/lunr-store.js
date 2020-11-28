96d6ed21c0bee5f12d95616058bbe16013f9e4b2
GitHub Action <action@github.com>
2020-11-28 23:44:13 +0000

    Build 8cc322d182

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,36 @@
+var store = [{
+        "title": "One paper accepted to WACV 2016",
+        "excerpt":"Our paper, “Sky Segmentation in the Wild: An Empirical Study”, was accepted to WACV 2016.  ","categories": [],
+        "tags": [],
+        "url": "https://www.zachbessinger.com/One-paper-accepted-to-WACV-2016/",
+        "teaser":null},{
+        "title": "One paper accepted to ICIP 2016",
+        "excerpt":"Our paper, “Quantifying Curb Appeal”, was accepted to ICIP 2016.  ","categories": [],
+        "tags": [],
+        "url": "https://www.zachbessinger.com/One-paper-accepted-to-ICIP-2016/",
+        "teaser":null},{
+        "title": "One paper accepted to ACM SIGSPATIAL 2016",
+        "excerpt":"Our paper, “Who Goes There? Approaches to Mapping Facial Appearance Diversity”, was accepted to ACM SIGSPATIAL 2016.  ","categories": [],
+        "tags": [],
+        "url": "https://www.zachbessinger.com/One-paper-accepted-to-ACM-SIGSPATIAL-2016/",
+        "teaser":null},{
+        "title": "One paper accepted to CVPR 2017",
+        "excerpt":"Our paper, “Predicting Ground-Level Scene Layout from Aerial Imagery”, was accepted to CVPR 2017.  ","categories": [],
+        "tags": [],
+        "url": "https://www.zachbessinger.com/One-paper-accepted-to-CVPR-2017/",
+        "teaser":null},{
+        "title": "One paper accepted to WACV 2019",
+        "excerpt":"Our paper, “A Generative Model of Worldwide Facial Appearance”, was accepted to WACV 2019.  ","categories": [],
+        "tags": [],
+        "url": "https://www.zachbessinger.com/One-paper-accepted-to-WACV-2019/",
+        "teaser":null},{
+        "title": "Defended my dissertation",
+        "excerpt":"I defended my dissertation!  ","categories": [],
+        "tags": [],
+        "url": "https://www.zachbessinger.com/defended-my-dissertation/",
+        "teaser":null},{
+        "title": "One paper accepted to JACR",
+        "excerpt":"Our paper, “Inconsistent AI: Deep learning models for breast cancer fail to deliver after closer inspection”, was accepted to the Journal of the American College of Radiology (impact factor: 3.785).  ","categories": [],
+        "tags": [],
+        "url": "https://www.zachbessinger.com/One-paper-accepted-to-JACR/",
+        "teaser":null}]
