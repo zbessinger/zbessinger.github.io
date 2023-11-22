b73d4fc8b5c2e40e7608e3bb3b0d817479618c49
GitHub Action <action@github.com>
2023-11-22 02:59:15 +0000

    Build 1c20ccba78

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,40 +2,54 @@ var store = [{
         "title": "One paper accepted to WACV 2016",
         "excerpt":"Our paper, “Sky Segmentation in the Wild: An Empirical Study”, was accepted to WACV 2016.  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/One-paper-accepted-to-WACV-2016/",
-        "teaser":null},{
+        "url": "/One-paper-accepted-to-WACV-2016/",
+        "teaser": null
+      },{
         "title": "One paper accepted to ICIP 2016",
         "excerpt":"Our paper, “Quantifying Curb Appeal”, was accepted to ICIP 2016.  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/One-paper-accepted-to-ICIP-2016/",
-        "teaser":null},{
+        "url": "/One-paper-accepted-to-ICIP-2016/",
+        "teaser": null
+      },{
         "title": "One paper accepted to ACM SIGSPATIAL 2016",
         "excerpt":"Our paper, “Who Goes There? Approaches to Mapping Facial Appearance Diversity”, was accepted to ACM SIGSPATIAL 2016.  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/One-paper-accepted-to-ACM-SIGSPATIAL-2016/",
-        "teaser":null},{
+        "url": "/One-paper-accepted-to-ACM-SIGSPATIAL-2016/",
+        "teaser": null
+      },{
         "title": "One paper accepted to CVPR 2017",
         "excerpt":"Our paper, “Predicting Ground-Level Scene Layout from Aerial Imagery”, was accepted to CVPR 2017.  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/One-paper-accepted-to-CVPR-2017/",
-        "teaser":null},{
+        "url": "/One-paper-accepted-to-CVPR-2017/",
+        "teaser": null
+      },{
         "title": "One paper accepted to WACV 2019",
         "excerpt":"Our paper, “A Generative Model of Worldwide Facial Appearance”, was accepted to WACV 2019.  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/One-paper-accepted-to-WACV-2019/",
-        "teaser":null},{
+        "url": "/One-paper-accepted-to-WACV-2019/",
+        "teaser": null
+      },{
         "title": "Defended my dissertation",
         "excerpt":"I defended my dissertation!  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/defended-my-dissertation/",
-        "teaser":null},{
+        "url": "/defended-my-dissertation/",
+        "teaser": null
+      },{
         "title": "One paper accepted to JACR",
         "excerpt":"Our paper, “Inconsistent AI: Deep learning models for breast cancer fail to deliver after closer inspection”, was accepted to the Journal of the American College of Radiology (impact factor: 3.785).  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/One-paper-accepted-to-JACR/",
-        "teaser":null},{
+        "url": "/One-paper-accepted-to-JACR/",
+        "teaser": null
+      },{
         "title": "One paper accepted to CVPR 2022",
         "excerpt":"Our paper, “LASER: LAtent SpacE Rendering for 2D Visual Localization”, was accepted to Conference on Computer Vision and Pattern Recognition (CVPR) and selected for full oral presentation at the conference.  ","categories": [],
         "tags": [],
-        "url": "https://www.zachbessinger.com/One-paper-accepted-to-CVPR-2022/",
-        "teaser":null}]
+        "url": "/One-paper-accepted-to-CVPR-2022/",
+        "teaser": null
+      },{
+        "title": "One paper accepted to WACV 2024",
+        "excerpt":"Our paper, “iBARLE: imBalance-Aware Room Layout Estimation”, was accepted to IEEE/CVF Winter Conference on Applications of Computer Vision (WACV).  ","categories": [],
+        "tags": [],
+        "url": "/One-paper-accepted-to-WACV-2024-copy/",
+        "teaser": null
+      }]
